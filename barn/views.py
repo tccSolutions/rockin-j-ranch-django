802cbbc74cfc616b1dba351a261cb412945b8b9c
tccSolutions <tim@tccs.tech>
2022-01-06 22:32:09 -0500

    folder restructure update to horse.html/views

--- barn/views.py
+++ barn/views.py
@@ -1,17 +1,12 @@
+import random
 from django.shortcuts import render
 from horse.models import Horse, Image
 # Create your views here.
 def barn(request):
-    horse_list = []
+   
     horses = Horse.objects.all()
     for horse in horses:       
-        new_horse = {
-            'id': horse.id,
-            'name': horse.name,
-            'about': horse.bio[0:150],
-            'adoptable': horse.adoptable,
-            'image': Image.objects.filter(horse=horse.id).first()
-        }
-        horse_list.append(new_horse)  
-    context = {'horses': horse_list}
+        horse.image = random.choice(Image.objects.filter(horse=horse.id))
+       
+    context = {'horses':horses}
     return render(request, 'barn/barn.html', context)
