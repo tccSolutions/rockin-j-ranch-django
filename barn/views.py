6a8ad319cf0abb10ceb1c26774d56c68011cf4d5
tccSolutions <tim@tccs.tech>
2022-01-22 14:08:12 -0500

    update to amigo

--- barn/views.py
+++ barn/views.py
@@ -7,8 +7,10 @@ from django.views.decorators.clickjacking import xframe_options_exempt
 def barn(request):
    
     horses = Horse.objects.all()
-    for horse in horses:       
-        horse.image = random.choice(Image.objects.filter(horse=horse.id))
+    for horse in horses: 
+        horse_images = Image.objects.filter(horse=horse.id);
+        if horse_images:      
+            horse.image = random.choice(Image.objects.filter(horse=horse.id))
            
     context = {'horses':horses}
     return render(request, 'barn/barn.html', context)
--- horse/admin.py
+++ horse/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Trainer, Horse, Training
+from .models import Trainer, Horse, Training,Image
 
 admin.site.register(Trainer)
 admin.site.register(Horse)
 admin.site.register(Training)
+admin.site.register(Image)
