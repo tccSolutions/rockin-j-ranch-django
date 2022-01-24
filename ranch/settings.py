feb518c549e15abf4a05fc9a041c5fa3a9ccc403
tccSolutions <tim@tccs.tech>
2022-01-24 07:43:10 -0500

    update to env variable

--- ranch/settings.py
+++ ranch/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^y^0pd3ce4u$6n@b5dxaricbx(f(7bswk2-vhkcuv^7amq63--'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
